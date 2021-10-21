b432e48004b389fe949aaf1ae570cea6eefaf658
ikram-maulana <ikram075@ummi.ac.id>
2021-10-21 08:23:13 +0700

    Melengkapi Fungsi insertBook

--- DCD/DicodingBooks/src/scripts/main.js
+++ DCD/DicodingBooks/src/scripts/main.js
@@ -1,5 +1,7 @@
 function main() {
 
+  const baseUrl = "https://web-server-book-dicoding.appspot.com";
+
   const getBook = () => {
     // tuliskan kode di sini!
     const xhr = new XMLHttpRequest();
@@ -17,7 +19,7 @@ function main() {
     }
 
     // Membuat GET request dan menetapkan target URL
-    xhr.open("GET", "https://web-server-book-dicoding.appspot.com/list");
+    xhr.open("GET", `${baseUrl}/list`);
     // Mengirimkan request
     xhr.send();
   };
@@ -25,6 +27,25 @@ function main() {
 
   const insertBook = (book) => {
     // tuliskan kode di sini!
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+      const responseJson = JSON.parse(this.responseText);
+      showResponseMessage(responseJson.message);
+      getBook();
+    }
+    xhr.onerror = function () {
+      showResponseMessage();
+    }
+
+    xhr.open("POST", `${baseUrl}/add`);
+
+    // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-Auth-Token", "12345");
+
+    // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+    xhr.send(JSON.stringify(book));
   };
 
   const updateBook = (book) => {
