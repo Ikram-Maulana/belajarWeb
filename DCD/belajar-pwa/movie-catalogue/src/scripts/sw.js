8919466196c1486baa8af79967ed5316ba67d590
Ikram-Maulana <ikram075@ummi.ac.id>
2021-11-10 21:51:54 +0700

    Menerapkan Cache pada Service Worker

--- DCD/belajar-pwa/movie-catalogue/src/scripts/sw.js
+++ DCD/belajar-pwa/movie-catalogue/src/scripts/sw.js
@@ -1,21 +1,19 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+const { assets } = global.serviceWorkerOption;
+
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
-
-  // TODO: Caching App Shell Resource
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
