21ad50cd5da5f5f6343d4df643173d9ee1e4a654
Ikram-Maulana <ikram075@ummi.ac.id>
2021-11-10 13:03:22 +0700

    Melakukan Request dari API

---
+++ DCD/belajar-pwa/movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'dce6844e0f35fb8114174fa318719de3',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
