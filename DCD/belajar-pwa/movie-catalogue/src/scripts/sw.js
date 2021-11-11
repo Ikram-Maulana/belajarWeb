8822fc131d20a7e0b0a1a4a84334eb22f04ce869
Ikram-Maulana <ikram075@ummi.ac.id>
2021-11-11 22:50:13 +0700

    Memberikan Fungsionalitas pada Tombol Like
    Memberikan Fungsionalitas pada Tombol Like and fixing chrome-extension cause by wappalyzer

--- DCD/belajar-pwa/movie-catalogue/src/scripts/sw.js
+++ DCD/belajar-pwa/movie-catalogue/src/scripts/sw.js
@@ -1,7 +1,9 @@
 import 'regenerator-runtime';
 import CacheHelper from './utils/cache-helper';
 
-const { assets } = global.serviceWorkerOption;
+const {
+  assets,
+} = global.serviceWorkerOption;
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('install', (event) => {
@@ -15,5 +17,9 @@ self.addEventListener('activate', (event) => {
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('fetch', (event) => {
+  console.log(event.request);
+
+  if (!(event.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
+
   event.respondWith(CacheHelper.revalidateCache(event.request));
 });
