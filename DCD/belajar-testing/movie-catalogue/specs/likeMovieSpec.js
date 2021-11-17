6a2b62b01ac9bd0af64176bc7aaadfb9bdc76884
Ikram Maulana <ikram075@ummi.ac.id>
2021-11-17 08:19:02 +0700

    Jagalah Kebersihan

--- DCD/belajar-testing/movie-catalogue/specs/likeMovieSpec.js
+++ DCD/belajar-testing/movie-catalogue/specs/likeMovieSpec.js
@@ -1,5 +1,5 @@
-import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+import * as TestFactories from './helpers/testFactories';
 
 describe('Liking A Movie', () => {
   const addLikeButtonContainer = () => {
@@ -11,33 +11,25 @@ describe('Liking A Movie', () => {
   });
 
   it('should show the like button when the movie has not been liked before', async () => {
-    await LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      movie: {
-        id: 1,
-      },
+    await TestFactories.createLikeButtonPresenterWithMovie({
+      id: 1,
     });
 
-    expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+    expect(document.querySelector('[aria-label="like this movie"]'))
+      .toBeTruthy();
   });
 
   it('should not show the unlike button when the movie has not been liked before', async () => {
-    await LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      movie: {
-        id: 1,
-      },
+    await TestFactories.createLikeButtonPresenterWithMovie({
+      id: 1,
     });
 
     expect(document.querySelector('[aria-label="unlike this movie"]')).toBeFalsy();
   });
 
   it('should be able to like the movie', async () => {
-    await LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      movie: {
-        id: 1,
-      },
+    await TestFactories.createLikeButtonPresenterWithMovie({
+      id: 1,
     });
 
     document.querySelector('#likeButton').dispatchEvent(new Event('click'));
@@ -51,11 +43,8 @@ describe('Liking A Movie', () => {
   });
 
   it('should not add a movie again when its already liked', async () => {
-    await LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      movie: {
-        id: 1,
-      },
+    await TestFactories.createLikeButtonPresenterWithMovie({
+      id: 1,
     });
 
     // Tambahkan film dengan ID 1 ke daftar film yang disukai
@@ -72,12 +61,8 @@ describe('Liking A Movie', () => {
     FavoriteMovieIdb.deleteMovie(1);
   });
 
-  // menggunakan metode xit, bukan it
   it('should not add a movie when it has no id', async () => {
-    await LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      movie: {},
-    });
+    await TestFactories.createLikeButtonPresenterWithMovie({});
 
     document.querySelector('#likeButton').dispatchEvent(new Event('click'));
 
