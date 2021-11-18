f0747efb59e2facc74b405b8a9d2028216f45b74
Ikram Maulana <ikram075@ummi.ac.id>
2021-11-18 10:03:59 +0700

    Aku Suka Apa Yang Aku Lihat

--- DCD/belajar-testing/movie-catalogue/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ DCD/belajar-testing/movie-catalogue/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -1,8 +1,6 @@
 /* eslint-disable class-methods-use-this */
 class FavoriteMovieSearchPresenter {
-  constructor({
-    favoriteMovies,
-  }) {
+  constructor({ favoriteMovies }) {
     this._listenToSearchRequestByUser();
     this._favoriteMovies = favoriteMovies;
   }
@@ -15,19 +13,23 @@ class FavoriteMovieSearchPresenter {
   }
 
   async _searchMovies(latestQuery) {
-    this._latestQuery = latestQuery;
-    const foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    this._latestQuery = latestQuery.trim();
+
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
 
     this._showFoundMovies(foundMovies);
   }
 
   _showFoundMovies(movies) {
-    const html = movies.reduce(
-      (carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`),
-      '',
-    );
+    const html = movies.reduce((carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`), '');
 
     document.querySelector('.movies').innerHTML = html;
+
     document.getElementById('movie-search-container')
       .dispatchEvent(new Event('movies:searched:updated'));
   }
